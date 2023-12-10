7a74825e96535a5efbc504ae6ea1e9824f5ceabf
hakozakitei <shinya.kudo.0711@gmail.com>
2023-12-10 21:39:28 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create,:edit, :update,:destroy]
   before_action :set_item, only: [:show, :edit, :update,:destroy]
   before_action :redirect_unless_author, only: [:edit, :update, :destroy]
+  before_action :redirect_if_sold_out, only: [:edit, :update]
 
 
   def index
@@ -54,4 +55,11 @@ class ItemsController < ApplicationController
   def redirect_unless_author
     redirect_to root_path unless current_user == @item.user
   end
+
+  def redirect_if_sold_out
+    if @item.purchase.present?
+      redirect_to root_path
+    end
+  end
+  
 end
