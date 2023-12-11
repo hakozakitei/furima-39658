d3585207787a63944838def1c0a3464e58c672b6
hakozakitei <shinya.kudo.0711@gmail.com>
2023-12-11 21:20:23 +0900

    修正３

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,11 +3,15 @@ class OrderForm
   attr_accessor :user_id, :item_id, :postal_code, :phone_number, :shipping_origin_id, :city, :address, :building_name, :token
 
   # バリデーションの定義
-  validates :user_id, :item_id, presence: true
-  validates :token, :postal_code, :phone_number, :shipping_origin_id, :city, :address, presence: true
+  with_options presence: true do
+  validates :user_id, :item_id
+  validates :token
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
   validates :shipping_origin_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :city
+  validates :address
+
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
